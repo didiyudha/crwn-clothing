1b276878565586a08b9997b9235a291af0a01208
didiyudha <didiyudha@gmail.com>
2020-05-21 22:35:43 +0700

    sign and sign up page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        <Route path="/sigin" component={SignInAndSignUpPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
