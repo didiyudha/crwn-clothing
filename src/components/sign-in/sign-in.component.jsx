79e18f02a30dc067187c7339722504aec5938e3b
didiyudha <didiyudha@gmail.com>
2020-05-21 23:20:05 +0700

    add button to login with firebase

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -50,6 +51,10 @@ export default class SignIn extends React.Component {
             required
           />
           <CustomButton type="submit">Sign in</CustomButton>
+          <CustomButton onClick={signInWithGoogle}>
+            {' '}
+            Sign in with Goole{' '}
+          </CustomButton>
         </form>
       </div>
     );
